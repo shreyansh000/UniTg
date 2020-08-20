2a510478587983e05ddacc4ea154dd39dc7e2d6c
shreyansh000 <55754035+shreyansh000@users.noreply.github.com>
2020-08-20 19:33:21 +0530

    Create plugin_data.py

---
+++ plugin_data.py
@@ -0,0 +1,38 @@
+
+import base64
+import dataclasses
+import dill
+import os
+
+
+def load_data(name: str) -> dict:
+    b64 = os.environ.pop(name, {})
+    if b64:
+        b64 = dill.loads(base64.b64decode(b64.encode()))
+    return b64
+
+
+def dump_data(instance) -> dict:
+    data_dict = {}
+    for i in dataclasses.fields(instance):
+        attr = getattr(instance, i.name, None)
+        if attr:
+            data_dict[i.name] = base64.b64encode(dill.dumps(attr)).decode()
+    return data_dict
+
+
+@dataclasses.dataclass
+class AFK:
+    privates: dict = None
+    groups: dict = None
+    sent: dict = None
+
+
+def dump_AFK() -> None:
+    cls_dict = dump_data(AFK)
+    if "privates" in cls_dict:
+        os.environ['userbot_afk_privates'] = cls_dict['privates']
+    if "groups" in cls_dict:
+        os.environ['userbot_afk_groups'] = cls_dict['groups']
+    if "sent" in cls_dict:
+        os.environ['userbot_afk_sent'] = cls_dict['sent']
