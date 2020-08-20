20dd0e3017b9d5fb05d78600b7c744037ae8fec1
shreyansh8184 <54529384+shreyansh8184@users.noreply.github.com>
2020-08-20 17:19:15 +0530

    Update corona_virus.py

--- stdplugins/corona_virus.py
+++ stdplugins/corona_virus.py
@@ -1,21 +1,73 @@
-"""CoronaVirus LookUp
-Syntax: .coronavirus <country>"""
-from covid import Covid
+import asyncio
+import aiohttp
 from uniborg.util import admin_cmd
 
+class AioHttp:
+    @staticmethod
+    async def get_json(link):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(link) as resp:
+                return await resp.json()
+
+    @staticmethod
+    async def get_text(link):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(link) as resp:
+                return await resp.text()
+
+    @staticmethod
+    async def get_raw(link):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(link) as resp:
+                return await resp.read()
+                
 @borg.on(admin_cmd(pattern="coronavirus (.*)", allow_sudo=True))
 async def _(event):
-    covid = Covid()
-    data = covid.get_data()
-    country = event.pattern_match.group(1)
-    country_data = get_country_data(country, data)
-    output_text = "" 
-    for name, value in country_data.items():
-        output_text += "`{}`: `{}`\n".format(str(name), str(value))
-    await event.edit("**CoronaVirus Info in {}**:\n\n{}".format(country.capitalize(), output_text))
-
-def get_country_data(country, world):
-    for country_data in world:
-        if country_data["country"] == country:
-            return country_data
-    return {"Status": "No information yet about this country!"}
+    args = event.pattern_match.group(1)
+    if not args:
+        try:
+            r = await AioHttp().get_json("https://corona.lmao.ninja/v2/all")
+            reply_text = f"""**Global Cases 🦠:**
+ - **Cases:** `{r['cases']:,}`
+ - **Cases Today:** `{r['todayCases']:,}`
+ - **Deaths:** `{r['deaths']:,}`
+ - **Deaths Today:** `{r['todayDeaths']:,}`
+ - **Recovered:** `{r['recovered']:,}`
+ - **Active:** `{r['active']:,}`
+ - **Critical:** `{r['critical']:,}`
+ - **Cases/Mil:** `{r['casesPerOneMillion']}`
+ - **Deaths/Mil:** `{r['deathsPerOneMillion']}``
+"""
+            await event.edit(f"{reply_text}")
+            return
+        except Exception as e:
+            await event.edit("`The corona API could not be reached`")
+            print(e)
+            await asyncio.sleep(3)
+            await message.delete()
+            return
+    country = args
+    r = await AioHttp().get_json(f"https://corona.lmao.ninja/v2/countries/{country}")
+    if "cases" not in r:
+        await event.edit("```The country could not be found!```")
+        await asyncio.sleep(3)
+        await message.delete()
+    else:
+        try:
+            reply_text = f"""**Cases for {r['country']} 🦠:**
+ - **Cases:** `{r['cases']:,}`
+ - **Cases Today:** `{r['todayCases']:,}`
+ - **Deaths:** `{r['deaths']:,}`
+ - **Deaths Today:** `{r['todayDeaths']:,}`
+ - **Recovered:** `{r['recovered']:,}`
+ - **Active:** `{r['active']:,}`
+ - **Critical:** `{r['critical']:,}`
+ - **Cases/Mil:** `{r['casesPerOneMillion']}`
+ - **Deaths/Mil:** `{r['deathsPerOneMillion']}`
+"""
+            await event.edit(f"{reply_text}")
+        except Exception as e:
+            await event.edit("`The corona API could not be reached`")
+            print(e)
+            await asyncio.sleep(3)
+            await message.delete()
